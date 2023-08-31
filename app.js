c3e7d487532cd05d5ea837ca6ecb771a0871af18
Mille Tedebring Thysell <milletedebring.thysell@elev.ga.ntig.se>
2023-08-31 11:01:18 +0200

    ✨ variabel kod

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log("Hello world!");
+console.log("Hello world!")
+
+let firstName
+
+firstName = "Mille"
+let lastName = "Thysell"
+let num = 23
+console.log(typeof (num))
+
+const Name = "Stina"
+console.log[Name]
+
+for (let i = 0; i < 10; i++){
+    const Name = "Sture" 
+    
+    console.log[Name]
+}
+
+var skola = "NTI"
+
+console.log(skola)
+
+let a = "jens"
+let b = 'jens'
+let c = `nmitt namn är ${a}`
+let d = 'mitt namn är ' + b 
+let e = 12
+let f = 16.7
+
+let g = true
+
+let h = false
+
+console.log(c, e, g)
